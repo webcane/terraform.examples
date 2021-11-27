6b9d03ae72cb105e8855ca5cb8e0d908b8cc587c
cane <webcane@ya.ru>
2021-11-27 18:49:19 +0300

    user data script

--- aws/book/main.tf
+++ aws/book/main.tf
@@ -14,6 +14,11 @@ provider "aws" {
 resource "aws_instance" "ami-example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
   tags = {
     "source" = "terraform"
     "Name"   = "tf-ami-example"
