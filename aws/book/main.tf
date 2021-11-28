18e9bc3e6416b1b0f7fc57b4d0e15042827c51d5
cane <webcane@ya.ru>
2021-11-28 12:02:22 +0300

    rn sec group

--- aws/book/main.tf
+++ aws/book/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_instance" "ami-example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.sg-example.id]
   user_data     = <<-EOF
               #!/bin/bash
               echo "${file("index.html")}" > index.html
@@ -26,12 +26,17 @@ resource "aws_instance" "ami-example" {
   }
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
+resource "aws_security_group" "sg-example" {
+  name = "terraform-sg-example"
+  description = "Allow public access"
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    "source" = "terraform"
+    "Name"   = "terraform-sg-example"
   }
 }
