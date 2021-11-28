5902d0ea18d1b1065cbbeffb47b9555727e54421
cane <webcane@ya.ru>
2021-11-28 11:20:44 +0300

    read template file

--- aws/book/main.tf
+++ aws/book/main.tf
@@ -11,16 +11,31 @@ provider "aws" {
   region = "us-east-2"
 }
 
+data "template_file" "hello-world-file" {
+  template = "${file("index.html")}"
+}
+
 resource "aws_instance" "ami-example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  user_data = <<-EOF
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data     = <<-EOF
               #!/bin/bash
-              echo "Hello, World" > index.html
+              echo "${data.template_file.hello-world-file.rendered}" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
   tags = {
     "source" = "terraform"
     "Name"   = "tf-ami-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
