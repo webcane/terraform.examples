4a54d1e7ab9d5d90676fd06a8b0a9df3804af0d7
cane <webcane@ya.ru>
2021-11-28 11:55:32 +0300

    file func

--- aws/book/main.tf
+++ aws/book/main.tf
@@ -11,17 +11,13 @@ provider "aws" {
   region = "us-east-2"
 }
 
-data "template_file" "hello-world-file" {
-  template = "${file("index.html")}"
-}
-
 resource "aws_instance" "ami-example" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
   user_data     = <<-EOF
               #!/bin/bash
-              echo "${data.template_file.hello-world-file.rendered}" > index.html
+              echo "${file("index.html")}" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
   tags = {
